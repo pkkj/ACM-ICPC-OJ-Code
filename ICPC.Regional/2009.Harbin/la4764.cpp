189facdbf1a829b2d461463dbd4e7002bd6fb556
pkkj <gz.pkkj@gmail.com>
2015-02-04 11:24:11 -0800

    new solutions

---
+++ ICPC.Regional/2009.Harbin/la4764.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cstring>
+#define N 100005
+int last[N], n;
+int solve(){
+	int best = 0, card;
+	memset(last, -1, sizeof(last));
+	for (int i = 0; i < n; i++){
+		scanf("%d", &card);
+		if (last[card] != -1){
+			int score = last[card] + (card == 999 ? 3 : 1);
+			if (score > best)
+				best = score;
+		}
+		last[card] = best;
+	}
+	return best;
+}
+
+int main(){
+	while (scanf("%d", &n) && n)
+		printf("%d\n", solve());
+}
